866c1dafc791160d2bf0b095aa6fbda1d7144e6c
Rodrigo Gutierrez <rgutierrez2090gmail.com>
2022-08-24 11:49:55 +0200

    refactorizando mapa

---
+++ game.js
@@ -0,0 +1,51 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+        
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
+
+function startGame() {
+    
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end';
+
+    const map = maps[1];
+    const mapRows = map.trim().split('\n');
+    const mapRowsCols = mapRows.map(row => row.trim().split(''));
+    console.log({map, mapRows, mapRowsCols});
+
+    mapRowsCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI + 1);
+            const posY = elementsSize * (rowI + 1);
+            game.fillText(emoji, posX, posY);
+        }); 
+    });
+
+    // for (let ver = 1; ver <= 10; ver++) {
+    //     for (let hor = 1; hor <= 10; hor++) {
+    //         game.fillText(emojis[mapRowsCols[ver - 1][hor - 1]], elementsSize * ver, elementsSize * hor);
+    //     }
+    // }
+}
+
